0576390c28d27530fd8898c4aa54ab1e8a74cd2a
Pantelis Giazitsis <giazitsispantelis@gmail.com>
2018-03-28 12:25:45 +0300

    initial functionality

--- LRUCache.swift
+++ LRUCache.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+fileprivate class CacheObject: NSObject {
+    var object: AnyObject?
+    var key: String?
+    weak var next: CacheObject?
+    weak var previous: CacheObject?
+}
+
+class LRUCache: NSObject {
+    private let capacity: Int
+    private let cache: [String : CacheObject] = [:]
+    
+    required init(capacity: Int) {
+        assert(capacity>1)
+        
+        self.capacity = capacity
+        
+        super.init()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(didReceiveMemoryWarning(notification:)), name: .UIApplicationDidReceiveMemoryWarning , object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: API
+    
+    func objectFor(key: String) -> AnyObject? {
+        guard let cacheObject = self.cache[key] else {return nil}
+        
+        return cacheObject.object
+    }
+    
+    // MARK: Notifications
+    @objc private func didReceiveMemoryWarning(notification: Notification) {
+        
+    }
+    
+}
